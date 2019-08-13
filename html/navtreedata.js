b462f5e488169f82f0933ca96aa6ee8a72702147
Travis CI <travis@travis-ci.org>
2019-08-13 20:57:34 +0000

    Deploy code docs to GitHub Pages Travis build: 3333
    Commit: 39ed916a55c0125ea47b6bb15b90123fdbc5b8aa

---
+++ html/navtreedata.js
@@ -0,0 +1,55 @@
+var NAVTREE =
+[
+  [ "OSSIA", "index.html", [
+    [ "Todo List", "todo.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Enumerations", "globals_enum.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html",
+"classopp_1_1node.html#aa4ba6cceb326ea94f58f30e5572d3c13",
+"classossia_1_1curve__abstract.html",
+"classossia_1_1net_1_1node__base.html#a59579d90d6d52e875bd7e4a3eed0c945",
+"classossia_1_1time__interval.html#a1315b2466d11d15683bcc16a60c730f1",
+"domain__base_8hpp.html#a12a5be18447a0ea568912a755ea95797",
+"group___c_node.html#ga4c5c5b145808f79b1e05cbfebf350b7d",
+"group___c_val.html#ga7b89a22e84f3c157809dc115daa0a92a",
+"node__attributes_8hpp.html#a9c37b0c0bddb499f44e9a1cf96e6794b",
+"structopp_1_1callback__index.html",
+"structossia_1_1oscquery_1_1osc__type__visitor.html#a55ea5bb71d99539cb003536eb7e0231e"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
